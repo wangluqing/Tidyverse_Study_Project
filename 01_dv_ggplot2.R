af12b68949ef18c06fcaf8e3f991650b1250b37b
wanglq <wangluqing360@163.com>
2021-11-10 22:36:49 +0800

    Tidyverse study

--- 01_dv_ggplot2.R
+++ 01_dv_ggplot2.R
@@ -90,3 +90,123 @@ ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
   geom_point(mapping = aes(color = class)) +
   geom_smooth()
 
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(mapping = aes(color = class)) +
+  geom_smooth(
+    data = filter(mpg, class == "subcompact"),
+    se = FALSE
+  )
+
+
+# 4 Statistical Transformations -------------------------------------------
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut))
+
+ggplot(data = diamonds) +
+  stat_count(mapping = aes(x = cut))
+
+demo <- tribble(
+  ~a, ~b,
+  "bar_1", 20,
+  "bar_2", 30,
+  "bar_3", 40
+)
+ggplot(data = demo) +
+  geom_bar(
+    mapping = aes(x = a, y = b), stat = "identity"
+  )
+
+ggplot(data = diamonds) +
+  geom_bar(
+    mapping = aes(x = cut, y = ..prop.., group = 1)
+  )
+
+ggplot(data = diamonds) +
+  stat_summary(
+    mapping = aes(x = cut, y = depth),
+    fun.ymin = min,
+    fun.ymax = max,
+    fun.y = median
+  )
+
+
+# 5 Position Adjustments --------------------------------------------------
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, color = cut))
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill = cut))
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill = clarity))
+
+
+ggplot(
+  data = diamonds,
+  mapping = aes(x = cut, fill = clarity)
+) +
+  geom_bar(alpha = 1/5, position = "identity")
+
+ggplot(
+  data = diamonds,
+  mapping = aes(x = cut, color = clarity)
+) +
+  geom_bar(fill = NA, position = "identity")
+
+ggplot(data = diamonds) +
+  geom_bar(
+    mapping = aes(x = cut, fill = clarity),
+    position = "fill"
+  )
+
+ggplot(data = diamonds) +
+  geom_bar(
+    mapping = aes(x = cut, fill = clarity),
+    position = "dodge"
+  )
+
+ggplot(data = mpg) +
+  geom_point(
+    mapping = aes(x = displ, y = hwy),
+    position = "jitter"
+  )
+
+
+
+# 6 Coordinate Systems ----------------------------------------------------
+
+ggplot(data = mpg, mapping = aes(x = class, y = hwy)) +
+  geom_boxplot()
+
+ggplot(data = mpg, mapping = aes(x = class, y = hwy)) +
+  geom_boxplot() +
+  coord_flip()
+
+nz <- map_data("nz")
+
+ggplot(nz, aes(long, lat, group = group)) +
+  geom_polygon(fill = "white", color = "black")
+
+ggplot(nz, aes(long, lat, group = group)) +
+  geom_polygon(fill = "white", color = "black") +
+  coord_quickmap()
+
+bar <- ggplot(data = diamonds) +
+  geom_bar(
+    mapping = aes(x = cut, fill = cut),
+    show.legend = FALSE,
+    width = 1
+  ) +
+  theme(aspect.ratio = 1) +
+  labs(x = NULL, y = NULL)
+
+bar + coord_flip()
+bar + coord_polar()
+
+
+# 7 The Layered Grammar of Graphics ---------------------------------------
+
+# ggplot2 做数据可视化
+
--- 03_dt_dplyr.R
+++ 03_dt_dplyr.R
@@ -1,3 +1,27 @@
+####################
 # 数据变换 dplyr包
+# R4DS 第3章
+###################
 
+library(nycflights13)
 library(tidyverse)
+
+
+# 1 nycflights13 ----------------------------------------------------------
+
+flights
+str(flights)
+glimpse(flights)
+
+flights %>% 
+  slice_head(n = 100) %>% 
+  View
+
+
+# 2 dplyr Basics ----------------------------------------------------------
+
+
+# 3 Filter Rows with filter() ---------------------------------------------
+
+
+
